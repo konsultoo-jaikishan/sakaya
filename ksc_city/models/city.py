ba8ea0c9a80a6f57f3bbf059b14045aa9a5c54aa
konsultoo-pratik <pratik.bavadiya@konsultoo.com>
2022-05-27 17:39:33 +0530

    [ADD] city and country of merchant on product

--- ksc_city/models/city.py
+++ ksc_city/models/city.py
@@ -4,18 +4,22 @@ from odoo import models, fields, api, _
 class Xcity(models.Model):
     _name = 'x_city'
     _description = 'City'
-    rec_name = 'x_name'
+    _rec_name = 'x_name'
 
     x_name = fields.Char(required=True, string="Name")
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    x_studio_merchant = fields.Many2one('res.partner', string="Merchant")
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     x_studio_city = fields.Many2one('x_city', string="City")
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    x_studio_merchant = fields.Many2one('res.partner', string="Merchant")
+    x_studio_merchant_city = fields.Many2one('x_city', string="Merchant - City",
+                                             related="x_studio_merchant.x_studio_city")
+    x_studio_merchant_country = fields.Many2one('res.country', string="Merchant - Country",
+                                                related="x_studio_merchant.country_id")
